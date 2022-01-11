d33b5dbe5ff8345b45c42b03dcc68e25eec7f64d
snsb08 <snsb08@gmail.com>
2022-01-11 11:52:44 -0500

    added functions

--- tic-tac-toe/__main__.py
+++ tic-tac-toe/__main__.py
@@ -10,11 +10,21 @@ Authors:
 """
 from tictactoe import player_control, create_player_board, player_winner, player_draw, display_board, start_tictactoe
 
+
 # The main function call other functions to run
 # This mean one entry point
 
 
 def main():
+    print("-------------------------------------------\nThe grid with integer values is an example\nplease choose from number\n-------------------------------------------")
+    player = player_control("")
+    board = create_player_board()
+    while not (player_winner(board) or player_draw(board)):
+        display_board(board)
+        start_tictactoe(player, board)
+        player = player_control(player)
+    display_board(board)
+    print("Winner of the game with three point")
 
 
 if __name__ == "__main__":
--- tic-tac-toe/tictactoe.py
+++ tic-tac-toe/tictactoe.py
@@ -1,19 +1,30 @@
 # The solution for the game create player board
 def create_player_board():
-
+    board = []
+    for square in range(9):
+        board.append(square + 1)
+    return board
 # Display the game board to the screen
 # The display_board hold one parameter pass it into grid
 def display_board(board):
-    
+    print()
+    print(f"{board[0]}|{board[1]}|{board[2]}")
+    print('-+-+-')
+    print(f"{board[3]}|{board[4]}|{board[5]}")
+    print('-+-+-')
+    print(f"{board[6]}|{board[7]}|{board[8]}")
+    print()
+
+
 # player control condition for two 'x' and 'o' set the input range
-def player_draw(board):
+#def player_draw(board):
 
 # The play winner of the game
-def player_winner(board):
+#def player_winner(board):
 
 # The player require input integer value
-def start_tictactoe(player, board):
+# def start_tictactoe(player, board):
 
 # The player are two 'x' and 'o'
 # Who the current player is 'X goes first'
-def player_control(player):
+# def player_control(player):
