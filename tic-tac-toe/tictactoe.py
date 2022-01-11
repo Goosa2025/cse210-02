bdd97aa7aee0fbf6947b4ebf6d233d45e135ca6d
owamamwen1 <87833174+owas1@users.noreply.github.com>
2022-01-11 18:18:37 +0100

    Add playercontrol startcontrol winnercontrol playerdraw

--- tic-tac-toe/tictactoe.py
+++ tic-tac-toe/tictactoe.py
@@ -6,6 +6,8 @@ def create_player_board():
     return board
 # Display the game board to the screen
 # The display_board hold one parameter pass it into grid
+
+
 def display_board(board):
     print()
     print(f"{board[0]}|{board[1]}|{board[2]}")
@@ -17,14 +19,38 @@ def display_board(board):
 
 
 # player control condition for two 'x' and 'o' set the input range
-#def player_draw(board):
+def player_draw(board):
+    for square in range(9):
+        if board[square] != "x" and board[square] != "o":
+            return False
+    return True
 
 # The play winner of the game
-#def player_winner(board):
+
+
+def player_winner(board):
+    return (board[0] == board[1] == board[2] or
+            board[3] == board[4] == board[5] or
+            board[6] == board[7] == board[8] or
+            board[0] == board[3] == board[6] or
+            board[1] == board[4] == board[7] or
+            board[2] == board[5] == board[8] or
+            board[0] == board[4] == board[8] or
+            board[2] == board[4] == board[6])
 
 # The player require input integer value
-# def start_tictactoe(player, board):
+
+
+def start_tictactoe(player, board):
+    square = int(input(f"{player}'s turn\nchoose a square (1-9): "))
+    board[square - 1] = player
 
 # The player are two 'x' and 'o'
 # Who the current player is 'X goes first'
-# def player_control(player):
+
+
+def player_control(player):
+    if player == "" or player == "o":
+        return "x"
+    elif player == "x":
+        return "o"
