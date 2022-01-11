7336699e213a8d63d90e456ea036c85052908b43
owamamwen1 <90800458+owamamwen1@users.noreply.github.com>
2022-01-11 16:13:51 +0100

    Update tictactoe.py

--- tic-tac-toe/tictactoe.py
+++ tic-tac-toe/tictactoe.py
@@ -1,29 +1,19 @@
 # The solution for the game create player board
 def create_player_board():
 
-    # Display the game board to the screen
-    # The display_board hold one parameter pass it into grid
-
-
+# Display the game board to the screen
+# The display_board hold one parameter pass it into grid
 def display_board(board):
-
-    # player control condition for two 'x' and 'o' set the input range
-
-
+    
+# player control condition for two 'x' and 'o' set the input range
 def player_draw(board):
 
-    # The play winner of the game
-
-
+# The play winner of the game
 def player_winner(board):
 
-    # The player require input integer value
-
-
+# The player require input integer value
 def start_tictactoe(player, board):
 
-    # The player are two 'x' and 'o'
-    # Who the current player is 'X goes first'
-
-
+# The player are two 'x' and 'o'
+# Who the current player is 'X goes first'
 def player_control(player):
